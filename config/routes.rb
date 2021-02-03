dc6c63c3b23a80c3da6c69748a479bd8a4a0ff6f
Erin Cola <colaerin@yahoo.com>
2021-02-03 08:44:09 -0500

    caretaker mvc created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :caretakers
   resources :events
   resources :pets
   resources :users
---
+++ test/controllers/caretakers_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class CaretakersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
