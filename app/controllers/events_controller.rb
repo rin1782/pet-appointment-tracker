9834a2e38f187548c453294a49bfbe1dff2c1d92
Erin Cola <colaerin@yahoo.com>
2021-02-03 21:46:36 -0500

    need to fix create action in events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,23 @@ class EventsController < ApplicationController
 
     def new
         @event = Event.new
+        @event.build_caretakers
+    end
+
+    def create 
+        @event = Event.new(event_params)
+        @event.pet_id = session[:pet_id]
+        if @event.save!
+            redirect_to event_path(@event)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def event_params
+        params.require(:event).permit(:name, :date, :caretaker_id, caretaker_attributes: [:title])
     end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
     belongs_to :pets
     belongs_to :caretakers
+    accepts_nested_attributes_for :caretakers
 end
